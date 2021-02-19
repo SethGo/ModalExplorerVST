303e44a46c300f791dea4a331d1f5849d8b1e196
SethG <48981838+SethG@users.noreply.github.com>
2021-02-19 13:20:36 -0700

    .gitignore

--- Source/MidiProcessor.h
+++ Source/MidiProcessor.h
@@ -2,15 +2,18 @@
 
 #include "JuceHeader.h"
 
-constexpr int intVal = 4;
-
 class MidiProcessor
 {
 public:
     void process (juce::MidiBuffer& midiMessages)
     {
         processedBuffer.clear();
-        
+        processMidiInput (midiMessages);
+        midiMessages.swapWith (processedBuffer);
+    }
+    
+    void processMidiInput (const juce::MidiBuffer& midiMessages)
+    {
         juce::MidiBuffer::Iterator it (midiMessages);
         juce::MidiMessage currentMessage;
         int samplePos;
@@ -19,24 +22,29 @@ public:
         {
             if (currentMessage.isNoteOnOrOff())
             {
-                auto transposedMessage  = currentMessage;
-                auto oldNoteNum = transposedMessage.getNoteNumber();
-                transposedMessage.setNoteNumber (oldNoteNum + 3); // min 3rd
-                processedBuffer.addEvent (transposedMessage, samplePos);
-                
-                auto transposedMessage2  = currentMessage;
-                auto oldNoteNum2 = transposedMessage2.getNoteNumber();
-                transposedMessage2.setNoteNumber (oldNoteNum2 + 7); // 5th
-                processedBuffer.addEvent (transposedMessage2, samplePos);
-                
-                auto transposedMessage3  = currentMessage;
-                auto oldNoteNum3 = transposedMessage3.getNoteNumber();
-                transposedMessage3.setNoteNumber (oldNoteNum3 + 14); // 9th
-                processedBuffer.addEvent (transposedMessage3, samplePos);
+                DBG(currentMessage.getNoteNumber());
+                addTransposedNotes (currentMessage, samplePos);
             }
             processedBuffer.addEvent (currentMessage, samplePos); // Root (user selected midi note)
         }
-        midiMessages.swapWith (processedBuffer);
+    }
+    
+    void addTransposedNotes (juce::MidiMessage currentMessage, int samplePos)
+    {
+        auto transposedMessage  = currentMessage;
+        auto oldNoteNum = transposedMessage.getNoteNumber();
+        transposedMessage.setNoteNumber (oldNoteNum + 3); // min 3rd
+        processedBuffer.addEvent (transposedMessage, samplePos);
+//        
+//        auto transposedMessage2  = currentMessage;
+//        auto oldNoteNum2 = transposedMessage2.getNoteNumber();
+//        transposedMessage2.setNoteNumber (oldNoteNum2 + 7); // 5th
+//        processedBuffer.addEvent (transposedMessage2, samplePos);
+//        
+//        auto transposedMessage3  = currentMessage;
+//        auto oldNoteNum3 = transposedMessage3.getNoteNumber();
+//        transposedMessage3.setNoteNumber (oldNoteNum3 + 14); // 9th
+//        processedBuffer.addEvent (transposedMessage3, samplePos);
     }
     juce::MidiBuffer processedBuffer;
 };
